9371f06c7943642bbd30b7021898f235bc77c9ce
miguel salazar <turutaperfecta@gmail.com>
2024-08-27 10:49:29 -0500

    nwe 7,8,9

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -20,8 +20,26 @@ class Todo:
 
 class TodoBook:
     def __init__(self):
-        self.todos: dict = {}
+        self.todos: dict[int, Todo] = {}
 
+    def add_todo(self, title: str, description: str) -> int:
+        todo_id: int = len(self.todos) + 1
+        todo: Todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = todo
+        return todo_id
 
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
 
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
 
+    def tags_todo_count(self) -> dict[str, int]:
+        tags_count: dict[str, int] = {}
+        for todo in self.todos.values():
+            for tag in todo.tags:
+                if tag in tags_count:
+                    tags_count[tag] += 1
+                else:
+                    tags_count[tag] = 1
+        return tags_count
